bfdd90dcd41b10a13c0287ddbca24f31e632df0a
Serena <whatsppnumber781@gmail.com>
2025-12-16 06:10:27 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))   # Owner ID (you)
+DB_PATH = "data.db"
